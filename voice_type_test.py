b88b17590e650ece6f794a16aa6018e1613c0c61
KeisukeTsuji <kei.iwmmd@gmail.com>
2020-04-12 19:41:12 +0900

    評価データを学習データ（ノイズ入りデータ）のクリーンなものに変更

--- voice_type_test.py
+++ voice_type_test.py
@@ -36,13 +36,25 @@ if __name__ == "__main__":
         train_label = np.append(train_label,'pull')
 
     # ここにテストデータを入れる
-    test_feature = get_feature('test_voice/light/light1.wav', nfft, nceps)
-    test_data = np.vstack((test_data, test_feature))
-    test_label = np.append(test_label, 'light')
+    test_voices = glob.glob(" no_processing_sound/voice/*.wav")
+    for test_voice in test_voices:
+        test_feature = get_feature(test_voice, nfft, nceps)
+        if len(test_data) == 0:
+            test_data = np.vstack((test_data, test_feature))
+        else:
+            test_data = np.vstack((test_data,test_feature))
+        test_label_name = test_voice.split('/')[2].split('_')[0]
+        test_label = np.append(test_label, test_label_name)
+
+        clf = SVC(kernel='linear', C=1).fit(train_data, train_label)
+        score = clf.score(test_data, test_label)
+
+        print(score)
+
 
     #特徴データをテキストに出力
-    feature_train_data=np.hstack((train_label.reshape(len(train_label),1),train_data))
-    feature_test_data = np.hstack((test_label.reshape(len(test_label), 1), test_data))
+    # feature_train_data=np.hstack((train_label.reshape(len(train_label),1),train_data))
+    # feature_test_data = np.hstack((test_label.reshape(len(test_label), 1), test_data))
     
     # with open("feature_data/train_data.txt","w") as f:
     #     writer=csv.writer(f) # writerオブジェクトを作成
@@ -53,8 +65,3 @@ if __name__ == "__main__":
     # with open("feature_data/test_data.txt","w") as f:
     #     writer=csv.writer(f) # writerオブジェクトを作成
     #     writer.writerows(feature_test_data) # feature_test_dataを書き込む
-    
-    clf = SVC(kernel='linear', C=1).fit(train_data, train_label)
-    score = clf.score(test_data, test_label)
-
-    print(score)
